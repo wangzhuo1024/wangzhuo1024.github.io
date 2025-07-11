5831ed472c91f5568d46bb696f268b9d8b6c4b81
wangzhuo1024 <3172359454@qq.com>
2025-07-11 21:43:23 +0800

    Site updated: 2025-07-11 21:43:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/10/Hello-world-0/","2025/07/10/hello-world/"];function toRandomPost(){
+var posts=["2025/07/10/hello-world/","2025/07/10/Hello-world-0/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
